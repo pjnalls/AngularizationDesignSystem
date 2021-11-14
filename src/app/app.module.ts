ff5bbc1444e13806186f022335d34ee38437cb0c
pjnalls <pjnalls.js@gmail.com>
2021-11-14 09:51:09 -0600

    `ng g c` components needed & add Storybook.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { FormButtonComponent } from './components/form-button/form-button.component';
+import { FormInputComponent } from './components/form-input/form-input.component';
+import { TitleComponent } from './components/title/title.component';
+import { ToDoComponent } from './components/to-do/to-do.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FormButtonComponent,
+    FormInputComponent,
+    TitleComponent,
+    ToDoComponent,
+    ToDoListComponent,
   ],
   imports: [
     BrowserModule,
