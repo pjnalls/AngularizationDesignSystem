ecd09e54304e0f8d78a1facf7375a946b66ccb35
pjnalls <pjnalls.js@gmail.com>
2021-11-14 14:32:40 -0600

    `import` new TODO components & ++ to declarations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { FormButtonComponent } from './components/form-button/form-button.component';
+import { FormInputComponent } from './components/form-input/form-input.component';
+import { TitleComponent } from './components/title/title.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    FormButtonComponent,
+    FormInputComponent,
+    TitleComponent,
+    TodoComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
