be7d6f8acad7505d8463d55091e810529683a2c1
pjnalls <pjnalls.js@gmail.com>
2021-11-17 03:02:30 -0600

    Add `todo` input to pass to `TodoList`'s child.

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  @Input() todos: Todo[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
