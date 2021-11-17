c95ea7cba22d6e407ecfec0c58a90edf0584373a
pjnalls <pjnalls.js@gmail.com>
2021-11-17 02:59:46 -0600

    Add `TodoComponent`'s implementation.

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  @Input() todo: Todo = {
+    text: '',
+    day: '',
+    reminder: true,
+  };
 
-  constructor() { }
+  @Output() onDeleteTodo: EventEmitter<Todo> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Todo> = new EventEmitter();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onDelete(todo: Todo) {
+    this.onDeleteTodo.emit(todo);
   }
 
+  onToggle(todo: Todo) {
+    this.onToggleReminder.emit(todo);
+  }
 }
