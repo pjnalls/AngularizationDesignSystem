9b417bfed37ac568ce5f37b5c94f0cf9d27775a2
pjnalls <pjnalls.js@gmail.com>
2021-11-17 13:58:41 -0600

    Simplify service.

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,37 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Todo } from 'src/app/Todo';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  }),
-};
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private apiUrl = 'http://localhost:5000/todo';
-
-  constructor(private http: HttpClient) {}
-
-  getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.apiUrl);
-  }
-
-  deleteTodo(todo: Todo): Observable<Todo> {
-    const url = `${this.apiUrl}/${todo.id}`;
-    return this.http.delete<Todo>(url);
-  }
-
-  updateTodoReminder(todo: Todo): Observable<Todo> {
-    const url = `${this.apiUrl}/${todo.id}`;
-    return this.http.put<Todo>(url, todo, httpOptions);
-  }
-
-  addTodo(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(this.apiUrl, todo, httpOptions);
-  }
+  constructor() {}
 }
