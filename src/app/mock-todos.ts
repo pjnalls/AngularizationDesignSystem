7843795472a7a1bcb600575ae0e3659fc953ae0a
pjnalls <pjnalls.js@gmail.com>
2021-11-17 12:44:31 -0600

    Update text and remove `reminder` properties.

--- src/app/mock-todos.ts
+++ src/app/mock-todos.ts
@@ -5,18 +5,15 @@ export const TODOS: Todo[] = [
     id: 1,
     text: 'Take out the trash.',
     day: '11/17 @ 7:00 AM',
-    reminder: true,
   },
   {
     id: 2,
-    text: 'I need to make a super long sentence to test this to-do app for the Angularization (A°) design system. This has been fun to build so far. Thanks for reading!',
+    text: 'I need to make a super long sentence to test this to-do app for the Angularization (A°) design system. We\'ll see how the wrap of the text for the to-do item looks in the UI.',
     day: '11/18 @ 12:00 PM',
-    reminder: true,
   },
   {
     id: 3,
     text: 'Go to doctor\'s appointment.',
     day: '11/19 @ 3:45 PM',
-    reminder: true,
   },
 ]
