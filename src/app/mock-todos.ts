dcea3dafafde01ba1ad48ffa92cc743bb34dbee5
pjnalls <pjnalls.js@gmail.com>
2021-11-17 02:54:53 -0600

    Create mock data for `Todo`s.

---
+++ src/app/mock-todos.ts
@@ -0,0 +1,22 @@
+import { Todo } from './Todo';
+
+export const TODOS: Todo[] = [
+  {
+    id: 1,
+    text: 'Take out the trash.',
+    day: '11/17 @ 7:00 AM',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'I need to make a super long sentence to test this to-do app for the Angularization (A°) design system. This has been fun to build so far. Thanks for reading!',
+    day: '11/18 @ 12:00 PM',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Go to doctor\'s appointment.',
+    day: '11/19 @ 3:45 PM',
+    reminder: true,
+  },
+]
