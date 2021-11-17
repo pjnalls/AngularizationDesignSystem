ffb8a7920c8e2a5689b6e4868a4a8c0406de7712
pjnalls <pjnalls.js@gmail.com>
2021-11-17 01:09:18 -0600

    Remove `FormComponent` implementation.

--- src/app/components/form-input/form-input.component.ts
+++ src/app/components/form-input/form-input.component.ts
@@ -1,49 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { UiService } from 'src/app/services/ui.service';
-import { Subscription } from 'rxjs';
-import { Todo } from 'src/app/Todo';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form-input',
   templateUrl: './form-input.component.html',
   styleUrls: ['./form-input.component.scss'],
 })
-export class FormInputComponent implements OnInit {
-  @Output() onAddTodo: EventEmitter<Todo> = new EventEmitter();
-  text: string = '';
-  day: string = '';
-  reminder: boolean = false;
-  showAddTodo: boolean = true;
-  subscription: Subscription = new Subscription();
+export class FormInputComponent {
+  @Input() text: string = '';
 
-  constructor(private uiService: UiService) {
-    this.subscription = this.uiService
-      .onToggle()
-      .subscribe((value) => (this.showAddTodo = value));
-  }
-
-  ngOnInit(): void {}
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-  onSubmit() {
-    if (!this.text) {
-      alert('Please add a task.');
-      return;
-    }
-
-    const newTodo = {
-      text: this.text,
-      day: this.day,
-      reminder: this.reminder,
-    };
-
-    this.onAddTodo.emit(newTodo);
-
-    this.text = '';
-    this.day = '';
-    this.reminder = false;
-  }
+  constructor() {}
 }
